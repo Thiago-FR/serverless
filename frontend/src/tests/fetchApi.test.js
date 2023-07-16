bb3d0eebaf9686329f3ebef9972c0a8a6330db77
Thiago-FR <thiago_ssfr@hotmail.com>
2023-07-16 18:51:33 -0300

    test: init tests

---
+++ frontend/src/tests/fetchApi.test.js
@@ -0,0 +1,35 @@
+import { auth, getData } from '../services/fetchApi.js';
+  import { authMock, getDataMock } from './mocks/fetchApi.mock.js';
+  
+  describe('Test FetchApi Auth', () => {
+    const EMAIL = 'teste@teste.com';
+    const ANO = '2020';
+
+    describe('Test fetchFindAll', () => {    
+      it('Test fetchFindAll - OK', async () => {
+        const EMAIL = 'teste@teste.com';
+
+        jest.spyOn(global, 'fetch');
+        global.fetch.mockResolvedValue({
+          json: jest.fn().mockResolvedValue(authMock),
+        });
+    
+        const result = await auth(EMAIL);
+
+        expect(EMAIL).toStrictEqual(result.email);
+      });
+    });
+  
+    describe('Test FetchApi getDate', () => {  
+      it('Test FetchApi - OK', async () => {
+        jest.spyOn(global, 'fetch');
+        global.fetch.mockResolvedValue({
+          json: jest.fn().mockResolvedValue(getDataMock),
+        });
+    
+        const result = await getData(EMAIL, ANO);
+
+        expect(result).toStrictEqual(getDataMock);
+      });
+    });
+  });
---
+++ frontend/src/tests/mocks/fetchApi.mock.js
@@ -0,0 +1,31 @@
+export const authMock = {
+    "id": "1",
+    "status": "ativo",
+    "email": "teste@teste.com",
+    "nome": "testenildo"
+}
+
+export const getDataMock = {
+    "result": {
+      "headcount": {
+        "id": "2022",
+        "color": "hsl(0, 100%, 100%)",
+        "data": [
+          {
+            "x": 1,
+            "y": 2
+          },
+        ]
+      },
+      "turnover": {
+        "id": "2022",
+        "color": "hsl(0, 100%, 100%)",
+        "data": [
+          {
+            "x": 2,
+            "y": 3
+          },
+          ]
+      }
+    }
+  }
